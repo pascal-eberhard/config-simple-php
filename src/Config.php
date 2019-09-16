8e3ec90ab581b7cbc1eecffbeaf5b9453efc11cb
Pascal Eberhard <pascal-eberhard-programming@posteo.de>
2019-09-16 23:13:22 +0200

    0000 First stable version, PENDING

--- src/Config.php
+++ src/Config.php
@@ -19,13 +19,20 @@ namespace PEPrograms\ConfigSimple;
  */
 class Config
 {
-    
+
     /**
      * Default charset
      *
      * @var string
      */
     const CHARSET = 'utf8';
+
+    /**
+     * UTF8 charset for internal use
+     *
+     * @var string
+     */
+    private const CHARSET_UTF8 = 'utf8';
 
     /**
      * Default directory path separator
@@ -49,11 +56,33 @@ class Config
     private static $baseDirSet = false;
 
     /**
-     * Base dir, windows format
+     * Base dir, real format
+     * For example, use linux path internally, at Windows
      *
      * @var string
      */
-    private static $baseDirWindows = '';
+    private static $baseDirReal = '';
+
+    /**
+     * Operating system key
+     *
+     * @var string
+     */
+    private static $operatingSystem = '';
+
+    /**
+     * Operating system, is Windows?
+     *
+     * @var bool
+     */
+    private static $operatingSystemIsWindows = false;
+
+    /**
+     * Operating system, is Windows, is set?
+     *
+     * @var bool
+     */
+    private static $operatingSystemIsWindowsIsSet = false;
 
     /**
      * Get base dir
@@ -64,34 +93,24 @@ class Config
      */
     public static function baseDir(): string
     {
-        throw new \LogicException('PENDING');
-        /** @var ProjectDirFinder\MethodInterface|null $finder */
-        $finder = null;
-        if (self::$baseDirSet) {
-            return self::$baseDir;
-        } elseif (!self::$baseDirSet) {
-            $finder = new ProjectDirFinder\ComposerClassLoaderAndReflection();
-            self::$baseDirSet = $finder->usable();
-        } elseif (!self::$baseDirSet) {
-            $finder = new ProjectDirFinder\ComposerClassLoader();
-            self::$baseDirSet = $finder->usable();
+        // Detect the operating system
+        self::operatingSystemIsWindows();
+
+        $path = __FILE__;
+
+        // To linux path format
+        if (self::$operatingSystemIsWindows) {
+            $path = self::pathWindowsToLinux($path);
         }
 
-        if (null === $finder) {
-            throw new \LogicException('No usable method found');
-        }
-        self::$baseDir = $finder->find();
-
-        # @todo complete
-
-        self::$baseDir = PathString\Utils::windowsToLinux(__FILE__);
+        // Delete the known project path suffix
         if ('/src/Config.php' !== mb_substr(self::$baseDir, -15, 15, Provider::CHARSET)) {
             throw new \UnexpectedValueException('Unexpected file path: ' . self::$baseDir);
         }
-
-        self::$baseDir = mb_substr(self::$baseDir, 0, -14, Provider::CHARSET);
-        self::$baseDirSet = true;
-        self::$baseDirWindows = PathString\Utils::linuxToWindows(self::$baseDir);
+//
+//        self::$baseDir = mb_substr(self::$baseDir, 0, -14, Provider::CHARSET);
+//        self::$baseDirSet = true;
+//        self::$baseDirReal = PathString\Utils::linuxToWindows(self::$baseDir);
 
         return self::$baseDir;
     }
@@ -101,15 +120,71 @@ class Config
      *
      * @return string
      */
-    public static function baseDirWindows(): string
+    public static function baseDirReal(): string
     {
         if (self::$baseDirSet) {
-            return self::$baseDirWindows;
+            return self::$baseDirReal;
         }
 
         self::baseDir();
 
-        return self::$baseDirWindows;
+        return self::$baseDirReal;
+    }
+
+    /**
+     * Is windows operating system?
+     * Than we have another ::baseDirReal(), iOS and linux path are the same syntax
+     *
+     * @return bool
+     * @throws \UnexpectedValueException
+     */
+    public static function operatingSystemIsWindows(): bool
+    {
+        if (self::$operatingSystemIsWindowsIsSet) {
+            return self::$operatingSystemIsWindows;
+        }
+
+        self::$operatingSystemIsWindows = ('windows' == mb_strtolower(\PHP_OS_FAMILY, self::CHARSET_UTF8));
+        if (self::$operatingSystemIsWindows && '\\' != \DIRECTORY_SEPARATOR) {
+            throw new \UnexpectedValueException('\\PHP_OS_FAMILY is "windows" (lower case), but'
+                . ' \\DIRECTORY_SEPARATOR is not ' . var_export('\\', true) . ', but '
+                . var_export(\DIRECTORY_SEPARATOR, true)
+            );
+        }
+
+        self::$operatingSystemIsWindowsIsSet = true;
+
+        return self::$operatingSystemIsWindows;
+    }
+
+    /**
+     * Convert windows to linux path
+     *
+     * @param string $path
+     * @return string
+     * @throws \InvalidArgumentException
+     */
+    public static function pathWindowsToLinux(string $path): string
+    {
+        // Must be absolute path
+        if ('' == $path) {
+            throw new \InvalidArgumentException('$path must not be empty');
+        }
+
+        // We need no convert, if $path not contains backslash
+        if (false === mb_strpos($path, '\\', 0, self::CHARSET_UTF8)) {
+            return $path;
+        }
+
+        // Convert
+        $path = preg_replace('/\\\\/iu', '/', $path);
+        if (':/' === mb_substr($path, 1, 2, self::CHARSET_UTF8)) {
+            $path = preg_replace_callback('/^([a-z]):\//iu', function ($match) {
+                return '/' . mb_strtolower($match[1], self::CHARSET_UTF8) . '/';
+            }, $path);
+        }
+
+        return $path;
     }
 // @todo Integrate simple versions
 //    /**
